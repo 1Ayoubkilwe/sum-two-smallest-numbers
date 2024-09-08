f2a847f11e230b7a0f5d7cb5aaa9af63e711d5ff
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 16:46:00 +0300

    answers\

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+function sumTwoSmallestNums(arr) {
+  
+    let sortedArr = arr.sort((a, b) => a - b);
+    return sortedArr[0] + sortedArr[1];
+  }
+  
+
+  console.log(sumTwoSmallestNums([12, 5, 24, 55, 18])); // 17
+  console.log(sumTwoSmallestNums([190, 300, 230, 800])); // 420
+  
